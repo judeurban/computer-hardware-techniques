3ca99b17635004277b9b281b35014f7348313cf3
Jude Urban <jude@utulsa.edu>
2023-08-27 16:02:18 -0500

    added source for custom-hello-world, blinking LED project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Computer Hardware Techniques
+
+ECE 4163
+
+### `.gitignore`
+
+I'm new to Quartus and FPGAs, so I've yet to determine which file types are just temporary log and compilation files. I'll leave the `.gitignore` empty for now.
---
+++ custom-hello-world/counter.v
@@ -0,0 +1,35 @@
+module counter(rst, clk, led);
+input rst, clk;
+output led;
+reg led;
+reg [31:0]cnt;
+
+always@(posedge clk or negedge rst)
+begin
+
+	// if reset pin is high
+	if(rst == 0)
+	begin
+		cnt <= 0;
+		led <= 0;
+	end
+
+	// reset pin is low, don't reset
+	else
+	begin
+	
+		// if count is greater than five hundred thousand, increment
+		if(cnt < 500000)
+		begin
+			cnt <= cnt + 1;
+		end
+		// reset
+		else
+		begin
+			cnt <= 0;
+			led <= ~led;
+		end
+	end
+end
+
+endmodule
