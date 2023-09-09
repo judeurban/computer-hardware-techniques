552a88de0675830a6c2f89a0a36033d94570db66
Jude Urban <jude@utulsa.edu>
2023-09-09 16:45:50 -0500

    connected all leds to their corresponding functions, everything works correctly

--- project1/core_logic.v
+++ project1/core_logic.v
@@ -1,14 +1,35 @@
-module core_logic(push_button, clock, led);
-input push_button, clock;
-output led;
-reg led;
-reg [31:0]cnt;
+module core_logic(
+	bit_0,
+	bit_1,
+	bit_2,
+	clock,
+	two_input_AND,
+	two_input_NAND,
+	three_input_OR,
+	two_input_NOR,
+	one_bit_Full_Adder,
+	two_by_one_MUX,
+	two_input_XOR
+);
 
-always@(posedge clock)
+// define I/O
+input clock;                    // basically the callback to the core logic
+input bit_0, bit_1, bit_2;      // inputs for the push buttons
+output two_input_AND, two_input_NAND, three_input_OR, two_input_NOR, one_bit_Full_Adder, two_by_one_MUX, two_input_XOR;
+
+// define I/O types
+reg two_input_AND, two_input_NAND, three_input_OR, two_input_NOR, one_bit_Full_Adder, two_by_one_MUX, two_input_XOR;
+
+always @(posedge clock)
 begin
-
-	led <= push_button;
-
+    // Assigning individual status for each led
+    two_input_AND <= bit_0 & bit_1;      					// 2-input AND
+    two_input_NAND <= ~(bit_0 & bit_1);   					// 2-input NAND
+    three_input_OR <= (bit_0 | bit_1 | bit_2);  			// 3-input OR
+    two_input_NOR <= ~(bit_0 | bit_1);   					// 2-input NOR
+    one_bit_Full_Adder <= (bit_0 ^ bit_1) ^ bit_2; 			// 1-bit Full Adder
+    two_by_one_MUX <= bit_2 ? bit_1 : bit_0;  				// 2:1 MUX
+    two_input_XOR <= bit_0 ^ bit_1;      					// 2-input XOR
 end
 
 endmodule
