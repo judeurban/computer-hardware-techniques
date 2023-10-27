f902c6f237486550ee0bbeb810a05bf4df919664
Jude Urban <jude@utulsa.edu>
2023-10-27 17:38:43 -0500

    added project 4 files, may need to remove temp files used by modelsim

---
+++ project4-testbench/alu-logic.vhd
@@ -0,0 +1,78 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- define the entity and ports (signals)
+entity alu_logic is
+    port(
+        -- bit array signals
+        mode_selector, nibble_a, nibble_b : in std_logic_vector(3 downto 0);
+        result : out std_logic_vector(3 downto 0)
+    );
+end entity alu_logic;
+
+architecture alu_logic_arch of alu_logic is
+    constant NOT_A:             std_logic_vector    :=  "0000";
+    constant NOT_A_OR_NOT_B:    std_logic_vector    :=  "0001";
+    constant NOT_A_AND_B:       std_logic_vector    :=  "0010";
+    constant LOGIC_0:           std_logic_vector    :=  "0011";
+    constant NOT_AB:            std_logic_vector    :=  "0100";
+    constant NOT_B:             std_logic_vector    :=  "0101";
+    constant A_XOR_B:           std_logic_vector    :=  "0110";
+    constant A_AND_NOT_B:       std_logic_vector    :=  "0111";
+    constant NOT_A_OR_B:        std_logic_vector    :=  "1000";
+    constant NOT_A_XOR_NOT_B:   std_logic_vector    :=  "1001";
+    constant B:                 std_logic_vector    :=  "1010";
+    constant A_AND_B:           std_logic_vector    :=  "1011";
+    constant LOGIC_1:           std_logic_vector    :=  "1100";
+    constant A_OR_NOT_B:        std_logic_vector    :=  "1101";
+    constant A_OR_B:            std_logic_vector    :=  "1110";
+    constant A:                 std_logic_vector    :=  "1111";
+begin
+    
+    process (mode_selector)
+    begin
+
+        -- switch on the mode selector
+        case mode_selector is
+            when NOT_A =>
+                result <= not nibble_a;
+            when NOT_A_OR_NOT_B =>
+                result <= not nibble_a or not nibble_b;
+            when NOT_A_AND_B =>
+                result <= not nibble_a and nibble_b;
+            when LOGIC_0 =>
+                result <= "0000"; -- Constant logic 0
+            when NOT_AB =>
+                result <= not (nibble_a and nibble_b);
+            when NOT_B =>
+                result <= not nibble_b;
+            when A_XOR_B =>
+                result <= nibble_a xor nibble_b;
+            when A_AND_NOT_B =>
+                result <= nibble_a and not nibble_b;
+            when NOT_A_OR_B =>
+                result <= not nibble_a or nibble_b;
+            when NOT_A_XOR_NOT_B =>
+                result <= (not nibble_a) xor (not nibble_b);
+            when B =>
+                result <= nibble_b;
+            when A_AND_B =>
+                result <= nibble_a and nibble_b;
+            when LOGIC_1 =>
+                result <= "1111"; -- Constant logic 1
+            when A_OR_NOT_B =>
+                result <= nibble_a or not nibble_b;
+            when A_OR_B =>
+                result <= nibble_a or nibble_b;
+            when A =>
+                result <= nibble_a;
+            when others =>
+                result <= "0000"; -- Just default case
+        end case;
+
+    end process;
+    
+end architecture;
+
---
+++ project4-testbench/tb-alu-logic.vhd
@@ -0,0 +1,158 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity alu_logic_tb is
+    end entity;
+
+architecture alu_logic_tb_arch of alu_logic_tb is
+
+    -- constant mode type declaration
+    constant NOT_A:             std_logic_vector    :=  "0000";
+    constant NOT_A_OR_NOT_B:    std_logic_vector    :=  "0001";
+    constant NOT_A_AND_B:       std_logic_vector    :=  "0010";
+    constant LOGIC_0:           std_logic_vector    :=  "0011";
+    constant NOT_AB:            std_logic_vector    :=  "0100";
+    constant NOT_B:             std_logic_vector    :=  "0101";
+    constant A_XOR_B:           std_logic_vector    :=  "0110";
+    constant A_AND_NOT_B:       std_logic_vector    :=  "0111";
+    constant NOT_A_OR_B:        std_logic_vector    :=  "1000";
+    constant NOT_A_XOR_NOT_B:   std_logic_vector    :=  "1001";
+    constant B:                 std_logic_vector    :=  "1010";
+    constant A_AND_B:           std_logic_vector    :=  "1011";
+    constant LOGIC_1:           std_logic_vector    :=  "1100";
+    constant A_OR_NOT_B:        std_logic_vector    :=  "1101";
+    constant A_OR_B:            std_logic_vector    :=  "1110";
+    constant A:                 std_logic_vector    :=  "1111";
+
+    -- static signals
+    constant nibble_a_tb: std_logic_vector := "1010";
+    constant nibble_b_tb: std_logic_vector := "1100";
+    
+    -- dynamic signals
+    signal result_tb : std_logic_vector(3 downto 0);
+    signal expected_result : std_logic_vector(3 downto 0);
+    signal mode_selector_tb : std_logic_vector(3 downto 0);
+    signal error_detection : std_logic_vector(3 downto 0);
+
+    -- declare a new component to use the alu logic entity
+    component alu_logic
+        port(
+            -- bit array signals
+            mode_selector, nibble_a, nibble_b : in std_logic_vector(3 downto 0);
+            result : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+begin
+
+    -- notA = 0101
+    -- notB = 0011
+    -- notA | notB = 0111
+
+    -- instantiate the alu-logic entity and map to internal signals
+    inst_alu_logic : alu_logic port map(
+        mode_selector => mode_selector_tb,
+        nibble_a => nibble_a_tb,
+        nibble_b => nibble_b_tb,
+        result => result_tb
+    );
+
+    alu_process : process
+    begin
+
+        -- test: NOT_A
+        mode_selector_tb <= NOT_A;
+        expected_result <= "0101";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+        
+        -- test: NOT_A_OR_NOT_B
+        mode_selector_tb <= NOT_A_OR_NOT_B;
+        expected_result <= "0011";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: NOT_A_AND_B
+        mode_selector_tb <= NOT_A_AND_B;
+        expected_result <= "0000";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: LOGIC_0
+        mode_selector_tb <= LOGIC_0;
+        expected_result <= "0000";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: NOT_AB
+        mode_selector_tb <= NOT_AB;
+        expected_result <= "0010";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: NOT_B
+        mode_selector_tb <= NOT_B;
+        expected_result <= "0011";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: A_XOR_B
+        mode_selector_tb <= A_XOR_B;
+        expected_result <= "0110";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: A_AND_NOT_B
+        mode_selector_tb <= A_AND_NOT_B;
+        expected_result <= "0010";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: NOT_A_OR_B
+        mode_selector_tb <= NOT_A_OR_B;
+        expected_result <= "1111";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: NOT_A_XOR_NOT_B
+        mode_selector_tb <= NOT_A_XOR_NOT_B;
+        expected_result <= "0000";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: B
+        mode_selector_tb <= B;
+        expected_result <= "1100";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: A_AND_B
+        mode_selector_tb <= A_AND_B;
+        expected_result <= "1000";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: LOGIC_1
+        mode_selector_tb <= LOGIC_1;
+        expected_result <= "1111";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: A_OR_NOT_B
+        mode_selector_tb <= A_OR_NOT_B;
+        expected_result <= "1011";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+        -- test: A_OR_B
+        mode_selector_tb <= A_OR_B;
+        expected_result <= "1110";
+        error_detection <= expected_result xor result_tb; 
+        wait for 50 ns;
+
+
+    end process;
+
+
+end alu_logic_tb_arch ; -- alu_logic_tb_arch
